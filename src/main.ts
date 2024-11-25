a064c3f441c9c683269817574e5cbb7bd4e18cee
Afif Hamzah <afifhamzah10@gmail.com>
2024-11-25 20:38:35 +0700

    Versioning untracked files

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
 import * as express from 'express';
 
-const port = process.env.PORT || 3000
-console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`)
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
@@ -19,6 +16,6 @@ async function bootstrap() {
   const uploadsPath = join(process.cwd(), 'public', 'uploads');
 
   app.use('/public/uploads', express.static(uploadsPath));
-  await app.listen(port);
+  await app.listen(3002);
 }
 bootstrap();
